7e466c689039b28e53d46b7a2dc8b723f706afc7
yuuma <yuuma0207@gmail.com>
2024-06-13 12:47:01 +0900

    add: login-image

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -51,72 +51,77 @@ export default function Signin() {
     const clearForm = () => {
         reset();
     }
-
+    
     return (
-        <div className="formContainer">
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <h1>ログイン</h1>
-            <hr />
-            <div className='uiForm'>
-            <p className="errorMsg">{errorMsg}</p>
-                <div className='formField'>
-                    <label htmlFor="userID">ユーザーID</label>
-                    <input 
-                        id = "userID"
-                        type="text" 
-                        placeholder='userID' 
-                        {...register('username', { 
-                            required: 'ユーザーIDを入力してください。', 
-                            maxLength: {
-                                value: 20,
-                                message: '20文字以内で入力してください。'
-                            },
-                            pattern: {
-                                value:
-                                    /^[A-Za-z0-9-]+$/i,
-                            message: 'ユーザーIDの形式が不正です。',
-                            }, 
-                        })}
-                    />
-                </div>
-                <ErrorMessage errors={errors} name="username" render={({message}) => <span>{message}</span>} />
-                <div className='formField'>
-                    <label htmlFor="password">パスワード</label>
-                    <input 
-                        id = "password"
-                        type="password" 
-                        placeholder='password' 
-                        role = 'password'
-                        {...register('password', { 
-                            required: 'パスワードを入力してください。', 
-                            maxLength: {
-                                value: 20,
-                                message: '20文字以内で入力してください',
-                            },
-                            pattern: {
-                                value:
-                                    /^[A-Za-z0-9]+$/i,
-                            message: 'パスワードの形式が不正です。',
-                            }, 
-                        })} 
-                    />
-                </div>
-                <ErrorMessage errors={errors} name="password" render={({message}) => <span>{message}</span>} />
-                <div className="loginButton">
-                    <button 
-                        type = "submit"
-                        className="submitButton"
-                        >ログイン
-                    </button>
-                    <button 
-                        type = "button"
-                        className="clearButton" 
-                        onClick={clearForm}
-                        >クリア
-                    </button>
-                </div>
+        <div className="container">
+            <div className="imageContainer">
+                <img src="/T-QARD.png" alt="t-qard" />
             </div>
-        </form>
+            <div className="formContainer">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h1>ログイン</h1>
+                <hr />
+                <div className='uiForm'>
+                <p className="errorMsg">{errorMsg}</p>
+                    <div className='formField'>
+                        <label htmlFor="userID">ユーザーID</label>
+                        <input 
+                            id = "userID"
+                            type="text" 
+                            placeholder='userID' 
+                            {...register('username', { 
+                                required: 'ユーザーIDを入力してください。', 
+                                maxLength: {
+                                    value: 20,
+                                    message: '20文字以内で入力してください。'
+                                },
+                                pattern: {
+                                    value:
+                                        /^[A-Za-z0-9-]+$/i,
+                                message: 'ユーザーIDの形式が不正です。',
+                                }, 
+                            })}
+                        />
+                    </div>
+                    <ErrorMessage errors={errors} name="username" render={({message}) => <span>{message}</span>} />
+                    <div className='formField'>
+                        <label htmlFor="password">パスワード</label>
+                        <input 
+                            id = "password"
+                            type="password" 
+                            placeholder='password' 
+                            role = 'password'
+                            {...register('password', { 
+                                required: 'パスワードを入力してください。', 
+                                maxLength: {
+                                    value: 20,
+                                    message: '20文字以内で入力してください',
+                                },
+                                pattern: {
+                                    value:
+                                        /^[A-Za-z0-9]+$/i,
+                                message: 'パスワードの形式が不正です。',
+                                }, 
+                            })} 
+                        />
+                    </div>
+                    <ErrorMessage errors={errors} name="password" render={({message}) => <span>{message}</span>} />
+                    <div className="loginButton">
+                        <button 
+                            type = "submit"
+                            className="submitButton"
+                            >ログイン
+                        </button>
+                        <button 
+                            type = "button"
+                            className="clearButton" 
+                            onClick={clearForm}
+                            >クリア
+                        </button>
+                    </div>
+                </div>
+            </form>
+            </div>
         </div>
   );
 }
